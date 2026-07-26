14cb520d314933d1b7b61530a70ff938767f03a9
mrmajdi <mrmajdi1993@gmail.com>
2026-07-27 02:41:15 +0330

    feat: add postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,2 @@
+// @vsc repo:vsc-project-113-frontend file:postcss.config.js task:f1-postcss-config-js module:frontend session:113
+module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
